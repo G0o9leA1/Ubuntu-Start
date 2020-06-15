f1e99ee8440f0bf928e4ac39caf057d215b71be5
z1xuan <li.z@wustl.edu>
2020-06-15 02:03:26 -0500

    Update ubuntu_18_desktop_start.sh

--- ubuntu_18_desktop_start.sh
+++ ubuntu_18_desktop_start.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+# script will terminate when error occurs
 set -e
 
 # Using apt mirror protocol
-# CN apt source: https://oss.link/config/apt-mirrors.txt
-# Reference: https://mritd.me/2019/03/19/how-to-set-multiple-apt-mirrors-for-ubuntu/
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
+# CN apt mirror source: https://oss.link/config/apt-mirrors.txt
+# Reference: https://mritd.me/2019/03/19/how-to-set-multiple-apt-mirrors-for-ubuntu/
 sudo sed -i -e 's/http:\/\/us.archive.ubuntu.com\/ubuntu\//mirror:\/\/oss.link\/config\/apt-mirrors.txt/' -e 's/http:\/\/us.archive.ubuntu.com\/ubuntu/mirror:\/\/oss.link\/config\/apt-mirrors.txt/' -e 's/http:\/\/security.ubuntu.com\/ubuntu/mirror:\/\/oss.link\/config\/apt-mirrors.txt/' /etc/apt/sources.list
+
+# US official mirror source: http://mirrors.ubuntu.com/mirrors.txt
+# Reference: https://linuxconfig.org/how-to-select-the-fastest-apt-mirror-on-ubuntu-linux
+# $ sudo sed -i -e 's/http:\/\/us.archive/mirror:\/\/mirrors/' -e 's/\/ubuntu\//\/mirrors.txt/' /etc/apt/sources.list
+
 sudo apt update
+sudo apt upgrade -y
 
 # Install common lib
 sudo apt install build-essential curl git -y
@@ -18,6 +25,7 @@ sudo apt install vim -y
 # Install chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P /tmp/
 sudo apt install /tmp/google-chrome-stable_current_amd64.deb
+rm -r /tmp/google-chrome-stable_current_amd64.deb
 
 
 # Install vscode
@@ -28,8 +36,10 @@ sudo apt install code
 # Install oh-my-zsh
 sudo apt install zsh -y
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-cd && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git && echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+chsh -s /bin/zsh
+cd && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git && echo "source ${PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
 
+# Create shared shell profile
 echo "if [ -f ~/.shell_profile ]; then" >> ~/.bashrc
 echo "    . ~/.shell_profile" >> ~/.bashrc
 echo "fi" >> ~/.bashrc
